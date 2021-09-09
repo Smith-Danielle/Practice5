584428fc88cc42b30edd5f4269244c4d35888051
Danielle Smith <smith.ddms@gmail.com>
2021-09-08 20:10:07 -0500

    Creation of Practice5

---
+++ Practice5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creation of Practice5");
+        }
+    }
+}
