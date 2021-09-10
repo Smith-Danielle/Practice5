d9f3e07c4872dd2f06e601688171fd9ba73e45fd
Danielle Smith <smith.ddms@gmail.com>
2021-09-09 20:28:34 -0500

    Codewars Java C# SQL Practice

--- Practice5/Program.cs
+++ Practice5/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
 
 namespace Practice5
 {
@@ -6,7 +12,25 @@ namespace Practice5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Creation of Practice5");
+            Console.WriteLine(digPow(3456789, 5));
+        }
+        public static long digPow(int n, int p)
+        {
+            var num = n.ToString().ToArray();
+            int power = p;
+            long solution = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                solution += Convert.ToInt64(Math.Pow(Convert.ToInt64(num[i]) -48, power));
+                power++;
+            }
+            double answer = Convert.ToDouble(solution) / Convert.ToDouble(n);
+            Console.WriteLine(answer);
+            if (answer % 1 == 0)
+            {
+                return Convert.ToInt64(answer);
+            }
+            return - 1;
         }
     }
 }
